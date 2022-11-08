ee2b21f4c5d76ac70d77d172244621e78a833593
brenley-woodard <brenleyew@gmail.com>
2022-11-08 12:48:30 -0800

    HP and default trolls showing on page load

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 /* Imports */
+import { renderTroll } from './render-utils.js';
 
 /* Get DOM Elements */
 const trollListEl = document.querySelector('.trolls');
 const formEl = document.querySelector('form');
 const elfHPEl = document.querySelector('#elf-hp');
 const defeatedNumEl = document.querySelector('#defeated-number');
+
 /* State */
 const trolls = [
     { id: 1, name: 'Tom', hp: 3 },
-    { id: 2, name: 'Tom', hp: 2 },
-    { id: 3, name: 'Tom', hp: 1 },
+    { id: 2, name: 'Bob', hp: 2 },
+    { id: 3, name: 'Flob', hp: 1 },
 ];
 /* Events */
 
 /* Display Functions */
+function displayTrolls() {
+    trollListEl.textContent = '';
 
+    for (let troll of trolls) {
+        const trollEl = renderTroll(troll);
+        trollListEl.append(trollEl);
+    }
+}
+
+displayTrolls();
 // (don't forget to call any display functions you want to run on page load!)
